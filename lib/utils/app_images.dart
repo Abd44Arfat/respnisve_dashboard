fcabfca580d84e78515a2a3d291876a0f6be861c
Abdelrahman arfat <abdoarfat924@gmail.com>
2023-12-27 17:42:31 +0200

    add images add font family add app colors and styles

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,55 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatear2
+  /// assets/images/avatear_2.svg
+  static const String imagesAvatear2 = "assets/images/avatear_2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.svg
+  static const String imagesCardBackground = "assets/images/card_background.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyInvestment
+  /// assets/images/my_investment.svg
+  static const String imagesMyInvestment = "assets/images/my_investment.svg";
+
+  /// Assets for imagesMyTransaction
+  /// assets/images/my_transaction.svg
+  static const String imagesMyTransaction = "assets/images/my_transaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
--- lib/views/dasboard_view.dart
+++ lib/views/dasboard_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:resbonsive_dashboard/widgets/adaptive_layaut_widget.dart';
+import 'package:resbonsive_dashboard/widgets/dashboard_desktop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+body: AdaptiveLayout(desktopLayout:(context)=> DashboardDesktopLayout(),mobileLayout: (context) => SizedBox(),tabletLayout:(context)=> SizedBox()),
+
+
+    );
   }
 }
