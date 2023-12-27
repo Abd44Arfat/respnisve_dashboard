8943485f21291fe4b71614fc6bcfd5c9b974553f
Abdelrahman arfat <abdoarfat924@gmail.com>
2023-12-27 11:35:37 +0200

    create dashboard view and add adabtive layout widget

---
+++ lib/views/adaptive_layaut_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+
+  @override
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrains) {
+      if (constrains.maxWidth < 600) {
+        return mobileLayout(context);
+      } else if (constrains.maxWidth < 900) {
+        return tabletLayout(context);
+      } else {
+        return desktopLayout(context);
+      }
+    });
+  }
+}
